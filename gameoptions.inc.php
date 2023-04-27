5bdb1afd99684a8acd6ba7a1dbd8b669f34373d0
Ori Avtalion <ori@avtalion.name>
2023-04-27 22:31:33 +0300

    WIP

---
+++ gameoptions.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *------
+ * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+ * Hotdog implementation : © Ori Avtalion <ori@avtalion.name>
+ *
+ * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
+ * See http://en.boardgamearena.com/#!doc/Studio for more information.
+ * -----
+ *
+ * gameoptions.inc.php
+ *
+ * Hotdog game options description
+ *
+ * In this file, you can define your game options (= game variants).
+ *
+ * Note: If your game has no variant, you don't have to modify this file.
+ *
+ * Note²: All options defined in this file should have a corresponding "game state labels"
+ *        with the same ID (see "initGameStateLabels" in hotdog.game.php)
+ *
+ * !! It is not a good idea to modify this file when a game is running !!
+ *
+ */
+
+$game_options = [
+    100 => [
+        'name' => totranslate('Footlong Variant'),
+        'values' => [
+            0 => ['name' => totranslate('No'), 'description' => totranslate('Play a regular game.')],
+            1 => ['name' => totranslate('Yes'), 'description' => totranslate('Allow choosing Footlong during bidding.')],
+        ],
+        'default' => 0
+    ],
+];
